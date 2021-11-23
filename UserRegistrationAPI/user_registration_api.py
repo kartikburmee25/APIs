8cdbe019097e7123176c5fcdac8c86f3ce03f318
Kartik Burmee <kartik@Kartiks-MacBook-Air.local>
2021-11-23 11:46:24 -0800

    Added password hashing and verification.

--- UserRegistrationAPI/user_registration_api.py
+++ UserRegistrationAPI/user_registration_api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import os, time, asyncio, sqlite3, smtplib
 from pydantic import BaseModel
 import json
+from authentication import (get_hashed_password,verify_password)
 
 class User(BaseModel):
 	first_name: str
@@ -12,7 +13,7 @@ class User(BaseModel):
 	confirm_password: str
 
 class UserCredentials(BaseModel):
-	user_name: str
+	username: str
 	password: str
 
 app = FastAPI()
@@ -32,7 +33,8 @@ async def send_email(email_address):
 
 async def save_user_to_db(user_info):
 
-	first_name,last_name,email_address,username,password = user_info['first_name'],user_info['last_name'],user_info['email_address'],user_info['username'],user_info['password']
+	first_name,last_name,email_address,username = user_info['first_name'],user_info['last_name'],user_info['email_address'],user_info['username']
+	password = get_hashed_password(user_info['password'])
 	values = (first_name,last_name,email_address,username,password)
 	conn = sqlite3.connect('user_info.db')
 	print ("Opened database successfully")
@@ -49,12 +51,13 @@ async def save_user_to_db(user_info):
 		conn.execute("INSERT INTO Users (FirstName,LastName,EmailAddress,UserName,Password) \
 			VALUES " + str(values) + ";")
 		conn.commit()
+		output = {'status':'ok', 'message':'User info added to database'}
 	except:
+		output = {'status':'error', 'message':'Unable to register, Please check user details'}
+	finally:
 		conn.close()
-	conn.close()
-	print("User info added to database")
 
-	return
+	return output
 
 @app.post("/api/v1/register_user")
 async def register_user(user_info: User):
@@ -74,26 +77,28 @@ async def login(user_credentials: UserCredentials):
 	response = {}
 	user_credentials = user_credentials.json()
 	user_credentials = json.loads(user_credentials)
-	username, password = user_credentials['username'], user_credentials['password']
-	values = (first_name,last_name,email_address,username,password)
+	username = user_credentials['username']
+	password = user_credentials['password']
 	conn = sqlite3.connect('user_info.db')
 	cursor = conn.cursor()
 	print ("Opened database successfully")
 	try:
 		query = '''SELECT Password FROM Users WHERE UserName= '{}'; '''.format(username)
 		cursor.execute(query)
+		results = cursor.fetchall()
+		if len(results)==0:
+			raise Exception
 	except:
 		conn.close()
-		return {'ERROR': 'Username does not exist. Please register as new user'}
+		return {'status':'Error', 'message':'Username does not exist. Please register as new user'}
 
-	results = cursor.fetchall()
 	actual_password = results[0][0]
 
-	if actual_password == password:
-		output = {'success':'Logged in successfully'}
+	if verify_password(password, actual_password):
+		output = {'status':'ok', 'message': 'Logged in successfully'}
 	else:
-		output = {'ERROR':'Sorry! incorrect password. Please try again'}
+		output = {actual_password:password,'status':'Error', 'message':'Sorry! incorrect password. Please try again'}
 
 	conn.close()
 
-	return
+	return output
